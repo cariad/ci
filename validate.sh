4b2949d5b5018eeb9b85fc03eed529f885e6e0f6
Cariad Eccleston <cariad@cariad.me>
2020-11-29 14:22:14 +0000

    Fix Dockerfile lint

--- validate.sh
+++ validate.sh
@@ -4,7 +4,7 @@ echo "Linting scripts..."
 find . -name '*.sh' -exec shellcheck -o all --severity style -x {} +
 
 echo "Linting Dockerfile..."
-hadolint
+hadolint Dockerfile
 
 echo "Linting YAML..."
 yamllint .
